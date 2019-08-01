9e0f0adb7ce33313fcf5064edc969a185695879a
chocobe <mjh2903@naver.com>
2019-08-01 22:59:53 +0900

    p355

--- SpaceShooter/Assets/02.Scripts/Enemy/EnemyAI.cs
+++ SpaceShooter/Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -8,7 +8,7 @@ public class EnemyAI : MonoBehaviour
     // 적 캐릭터의 상태를 표한하기 위한 열거형 변수 정의
     public enum State
     {
-        PATROL, TRACE, ATTACK,DIE
+        PATROL, TRACE, ATTACK, DIE
     }
     // 상태를 저장할 변수
     public State state = State.PATROL;
@@ -17,6 +17,7 @@ public class EnemyAI : MonoBehaviour
     private Transform playerTr;
     // 적 캐릭터의 위치를 저장할 변수 
     private Transform enemyTr;
+    private Animator animator; // Animator 컴포넌트를 저장할 변수
 
     // 공격 사정거리
     public float attackDist = 5.0f;
@@ -29,7 +30,13 @@ public class EnemyAI : MonoBehaviour
     // 코루틴에서 사용할 지연시간 변수
     private WaitForSeconds ws;
 
-    private void Awake()
+    // 이동을 제어하는 MoveAgent 클래스를 저장할 변수
+    private MoveAgent moveAgent;
+    // 애니메이터 컨트롤러에 정의한 파라미터의 해시값을 미리 추출
+    private readonly int hashMove = Animator.StringToHash("IsMove");
+    private readonly int hashSpeed = Animator.StringToHash("Speed");
+
+    private void Awake() // 가장 먼저 수행되는 함수 
     {
         // 주인공 게임오브젝트 추출
         var player = GameObject.FindGameObjectWithTag("PLAYER");
@@ -38,14 +45,17 @@ public class EnemyAI : MonoBehaviour
             playerTr = player.GetComponent<Transform>();
         // 적 캐릭터의 Transform 컴포넌트 추출
         enemyTr = GetComponent<Transform>();
+        animator = GetComponent<Animator>(); // Animator 컴포넌트 추출
 
         // 코루틴의 지연시간 생성
         ws = new WaitForSeconds(0.3f);
+        moveAgent = GetComponent<MoveAgent>(); // 이동을 제어하는 MoveAgent 클래스를 추출
     }
-    void OnEnable()
+    void OnEnable() // Awake 이후 Statr 이전에 실행되는 함수
     {
         // CheckState 코루틴 함수 실행
         StartCoroutine(CheckState());
+        StartCoroutine(Action()); // Action 코루틴 실행
     }
     IEnumerator CheckState()
     {
@@ -57,12 +67,12 @@ public class EnemyAI : MonoBehaviour
             float dist = Vector3.Distance(playerTr.position, enemyTr.position); // 주인공과 적 캐릭터 간의 거리를 계산
 
             // 공격 사정거리 이내인 경우
-            if(dist <= attackDist)
+            if (dist <= attackDist)
             {
                 state = State.ATTACK;
             }
             // 추적 사정거리 이내인 경우 
-            else if(dist <= traceDist)
+            else if (dist <= traceDist)
             {
                 state = State.TRACE;
             }
@@ -75,6 +85,33 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    IEnumerator Action()
+    {
+        while(!isDie) // 적 캐릭터가 사망할 때 까지 무한루프
+        {
+            yield return ws; 
+
+            switch(state) // 상태에 따라 분기 처리
+            {
+                case State.PATROL: // 순찰 모드 활성화
+                    moveAgent.patrolling = true;
+                    animator.SetBool(hashMove, true);
+                    break;
+                case State.TRACE: // 주인공의 위치를 넘겨 추적 모드로 변경
+                    moveAgent.traceTarget = playerTr.position;
+                    animator.SetBool(hashMove, true);
+                    break;
+                case State.ATTACK: // 순찰 및 추적을 정지
+                    moveAgent.Stop();
+                    animator.SetBool(hashMove, false);
+                    break;
+                case State.DIE:
+                    moveAgent.Stop();
+                    break;
+
+            }
+        }
+    }
     void Start()
     {
         
@@ -83,6 +120,6 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat(hashSpeed, moveAgent.speed); // Speed 파라미터에 이동 속도를 전달 
     }
 }
--- SpaceShooter/Assets/02.Scripts/Enemy/MoveAgent.cs
+++ SpaceShooter/Assets/02.Scripts/Enemy/MoveAgent.cs
@@ -11,12 +11,54 @@ public class MoveAgent : MonoBehaviour
     public List<Transform> wayPoints; //  순찰 지점들을 저장하기 위한 List 타입 변수
     public int nextIdx; // 다음 순찰 지점의 배열의 인덱스
 
+    private readonly float patrolSpeed = 1.5f;
+    private readonly float traceSpeed = 4.0f;
+    private float damping = 1.0f; // 회전할 때의 속도를 조절하는 계수
+
     private NavMeshAgent agent; // NavMeshAgent 컴포넌트를 저장할 변수
+    private Transform enemyTr; // 적 캐릭터의 Transform 컴포넌트를 저장할 변수
+
+    private bool _patrolling; // 순찰 여부를 판단하는 변수
+    public bool patrolling // patrolling 프로퍼티 정의 (getter, setter)
+    {
+        get { return _patrolling; }
+        set
+        {
+            _patrolling = value;
+            if(_patrolling)
+            {
+                agent.speed = patrolSpeed;
+                damping = 1.0f; // 순찰 상태의 회전 계수
+                MoveWayPoint();
+            }
+        }
+    }
+
+    private Vector3 _traceTarget; //추적 대상의 위치를 저장하는 변수
+    public Vector3 traceTarget
+    {
+        get { return _traceTarget; }
+        set
+        {
+            _traceTarget = value;
+            agent.speed = traceSpeed;
+            damping = 7.0f; // 추적 상태의 회전 계수
+            TraceTarget(_traceTarget);
+        }
+    }
+
+    public float speed
+    {
+        get { return agent.velocity.magnitude; }
+    }
     void Start()
     {
+        enemyTr = GetComponent<Transform>(); // 적 캐릭터의 Transform 컴포넌트 추출 후 변수에 저장
         // NavMeshAgent 컴포넌트를 추출한 후 변수에 저장
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = false; // 목적지에 가까워질수록 속도를 줄이는 옵션 비활성화
+        agent.updateRotation = false; // 자동으로 회전하는 기능을 비활성화
+        agent.speed = patrolSpeed; // 속도는 순찰 속도로 초기화
 
         var group = GameObject.Find("WayPointGroup"); // 하이러라키 뷰의 WayPointGroup 게임 오브젝트를 추출
         if(group != null)
@@ -37,9 +79,28 @@ public class MoveAgent : MonoBehaviour
         // 내비게이션 기능을 활성화해서 이동을 시작함
         agent.isStopped = false;
     }
+    void TraceTarget(Vector3 pos) // 주인공을 추적할 때 이동시키는 함수
+    {
+        if (agent.isPathStale) return;
+        agent.destination = pos;
+        agent.isStopped = false;
+
+    }
+    public void Stop() // 순찰 및 추적을 정지시키는 함수
+    {
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero; // 바로 정지하기 위해 속도를 0으로 설정
+        _patrolling = false;
+    }
     // Update is called once per frame
     void Update()
     {
+        if(agent.isStopped == false) // 적 캐릭터가 이동 중일 때만 회전
+        {
+            Quaternion rot = Quaternion.LookRotation(agent.desiredVelocity); // NavMeshAgent가 가야 할 방향 벡터를 쿼터니언 타입의 각도로 변환
+            enemyTr.rotation = Quaternion.Slerp(enemyTr.rotation, rot, Time.deltaTime * damping); // 보간 함수를 사용해 점진적으로 회전 시킴
+        }
+        if (_patrolling) return; // 순찰 모드가 아닐 경우 이후 로직을 수행하지 않음
         // NavMeshAgent가 이동하고 있고 목적지에 도착했는지 여부를 계산
         if(agent.velocity.sqrMagnitude >= 0.2f * 0.2f && agent.remainingDistance <= 0.25f)
         {
